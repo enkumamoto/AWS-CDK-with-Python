e12f1a4bc4b74c0b080ac0875422fb2d8c3cb2c2
enkumamoto <eiji.kumamoto@gmail.com>
2024-05-18 12:20:54 -0300

    Upload

--- SERVERLESS_APP-LAMBDATODYNAMODB/serverless_app_lambdatodynamodb/serverless_app_lambdatodynamodb_stack.py
+++ SERVERLESS_APP-LAMBDATODYNAMODB/serverless_app_lambdatodynamodb/serverless_app_lambdatodynamodb_stack.py
@@ -8,40 +8,32 @@ from aws_cdk import (
     aws_cloudwatch as cloudwatch
 )
 from constructs import Construct
-from aws_solutions_cpnsstructions.aws_lambda_dynamodb import LambdaToDynamoDB
+from aws_solutions_constructs.aws_lambda_dynamodb import LambdaToDynamoDB
 
-class ServerlessAppStack(Stack):
+class ServerlessAppLambdatodynamodbStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        products_backend = LambdaToDynamoDB (self, "ProductBackend",
+        products_backend = LambdaToDynamoDB(self, 'ProductBackend',
                                             lambda_function_props = lambda_.FunctionProps(
-                                                code = lambda_.Code.from_asset("lambda_src"),
-                                                handler = "product_backend_function.lambda_handler",
+                                                code = lambda_.Code.from_asset('lambda_src'),
+                                                handler = 'product_list_function.lambda_handler',
                                                 runtime = lambda_.Runtime.PYTHON_3_10,
                                             ),
-                                            table_environment_variable_name = "TABLE_NAME",
-                                            table_permission = "Read") # Garante permissões ao DynamoDB
-
-        products_table = dynamodb.Table (self, "ProductsTable",
-                                        partition_key = dynamodb.Attribute(
-                                            name = "id",
-                                            type = dynamodb.AttributeType.STRING
-                                        ),
-                                        billing_mode = dynamodb.BillingMode.PAY_PER_REQUEST,
-                                        removal_policy = RemovalPolicy.DESTROY)
+                                            table_environment_variable_name='TABLE_NAME',
+                                            table_permissions='Read') # Garante permissões ao DynamoDB
 
         products_table = products_backend.dynamo_table
 
         product_list_function = products_backend.lambda_function
 
-        products_table.apply_removal_policy(RemovalPolicy)
+        # Deleta a tabela na pilha de exclusão
+        products_table.apply_removal_policy(RemovalPolicy.DESTROY)
 
         # Adicionando uma URL Lambda pa a Função Lambda para executa via internet
         product_list_url = product_list_function.add_function_url(
-            auth_type = lambda_.FunctionUrlAuthType.NONE
-            )
+            auth_type = lambda_.FunctionUrlAuthType.NONE)
         
         # Adicionando um output stack para acessar facilmente a função URL
         CfnOutput(self, "ProductUrl",
--- SERVERLESS_APP_LambdaToDynamoDB/README.md
+++
@@ -1,7 +0,0 @@
-# About / Sobre
-
-This folder is about DynamoDB
-
----
-
-Este diretório é sobre DynamoDB
