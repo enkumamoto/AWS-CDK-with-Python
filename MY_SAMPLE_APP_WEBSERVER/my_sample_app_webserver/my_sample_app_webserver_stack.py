a44c8a3598dfa0b6153b0323977ac5c5cdcd9209
enkumamoto <eiji.kumamoto@gmail.com>
2024-05-19 15:38:22 -0300

    Upload

--- MY_SAMPLE_APP_WEBSERVER/my_sample_app_webserver/my_sample_app_webserver_stack.py
+++ MY_SAMPLE_APP_WEBSERVER/my_sample_app_webserver/my_sample_app_webserver_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     Stack,
     CfnOutput,
-    aws_ec2 as ec2
+    aws_ec2 as ec2,
+    aws_s3_assets as s3_assets
 )
 from constructs import Construct
 
@@ -31,7 +32,7 @@ class MySampleAppWebserverStack(Stack):
         # Instalação dos pacote durante lançamento
         web_server.add_user_data("yum update -y",
                                  "amazon-linux-extras install nginx1",
-                                 "service nginx start")
+                                 "rm -rf /usr/share/nginx/html/*")
         
         CfnOutput(self, "WebServerDNS",
                   value = web_server.instance_public_dns_name)
@@ -40,3 +41,16 @@ class MySampleAppWebserverStack(Stack):
         # Permissão para conectar ao WebServer
         web_server.connections.allow_from_any_ipv4(ec2.Port.tcp(80), "Allow HTTP access from the Internet")
         web_server.connections.allow_from_any_ipv4(ec2.Port.tcp(22), "Allow SSH access from the Internet")
+
+        # Deploy de web page no webserver
+        web_page_asset = s3_assets.Asset(self, "WebPageAsset",
+                                         path = "web_pages/index.html")
+        
+        web_server.user_data.add_s3_download_command(bucket = web_page_asset.bucket,
+                                                     bucket_key = web_page_asset.s3_object_key,
+                                                     local_file = '/usr/share/nginx/html/index.html')
+        
+        web_page_asset.grant_read(web_server.role)
+
+
+        web_server.add_user_data("service start nginx")
