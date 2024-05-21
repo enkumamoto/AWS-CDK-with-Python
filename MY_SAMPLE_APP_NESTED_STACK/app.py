abe90343166789d826c98824271a21401c545628
enkumamoto <eiji.kumamoto@gmail.com>
2024-05-21 06:40:46 -0300

    Upload

--- MY_SAMPLE_APP_NESTED_STACK/app.py
+++ MY_SAMPLE_APP_NESTED_STACK/app.py
@@ -4,25 +4,16 @@ import os
 import aws_cdk as cdk
 
 from my_sample_app_nested_stack.my_sample_app_nested_stack_stack import MySampleAppNestedStackStack
+from my_sample_app_nested_stack.network_stack import NetworkStack
 
 
 app = cdk.App()
-MySampleAppNestedStackStack(app, "MySampleAppNestedStackStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+root_stack = cdk.Stack(app, "RootStack")
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+network_stack = NetworkStack(root_stack, "NetworkStack")
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+MySampleAppNestedStackStack(app, "MySampleAppNestedStackStack", 
+                            my_vpc=network_stack.vpc)
 
 app.synth()
