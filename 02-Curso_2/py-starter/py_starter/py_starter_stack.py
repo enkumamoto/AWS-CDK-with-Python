4bbfee70e3e0dc173a1b20b72d96d39f7bdee23b
enkumamoto <eiji.kumamoto@gmail.com>
2024-05-30 17:13:32 -0300

    Upload

--- 02-Curso_2/py-starter/py_starter/py_starter_stack.py
+++ 02-Curso_2/py-starter/py_starter/py_starter_stack.py
@@ -1,20 +1,20 @@
 from aws_cdk import (
     aws_ec2 as ec2,
     Stack,
-    aws_sns as sns,
-    aws_lambda as lambda_,
-    aws_apigateway as apigw,
-    aws_cloudwatch as cloudwatch,
-    aws_cloudfront as cloudfront,
-    aws_dynamodb as dynamodb,
     aws_s3 as s3,
-    aws_s3_assets as s3_assets,
-    aws_route53 as route53,
-    # aws_sqs as sqs,
+    Duration,
 )
+
 from constructs import Construct
 
 class PyStarterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        s3.Bucket(self, "TestBucket",
+                  lifecycle_rules = [
+                    s3.LifecycleRule(
+                        expiration = Duration.days(3)
+                    )
+                  ])
