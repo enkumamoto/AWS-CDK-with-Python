6f6509ea360fb812d2787e94f692b2243186bc2b
enkumamoto <eiji.kumamoto@gmail.com>
2024-05-30 17:29:48 -0300

    Upload

--- 02-Curso_2/py-starter/py_starter/py_starter_stack.py
+++ 02-Curso_2/py-starter/py_starter/py_starter_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     Stack,
     aws_s3 as s3,
     Duration,
+    CfnOutput,
 )
 
 from constructs import Construct
@@ -12,9 +13,12 @@ class PyStarterStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        s3.Bucket(self, "TestBucket",
+        bucket = s3.Bucket(self, "TestBucket",
                   lifecycle_rules = [
                     s3.LifecycleRule(
                         expiration = Duration.days(3)
                     )
-                  ])
+                  ]
+                )
+        CfnOutput(self, "BucketName", 
+                  value = bucket.bucket_name)
