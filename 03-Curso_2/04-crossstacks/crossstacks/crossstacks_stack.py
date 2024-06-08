01823e9864916ca1b59570c4de36ed9492db5cd8
enkumamoto <eiji.kumamoto@gmail.com>
2024-06-08 14:59:28 -0300

    Upload

--- 03-Curso_2/04-crossstacks/crossstacks/crossstacks_stack.py
+++ 03-Curso_2/04-crossstacks/crossstacks/crossstacks_stack.py
@@ -1,39 +1,53 @@
+# Importa módulos específicos do pacote aws_cdk
 from aws_cdk import (
-    aws_ec2 as ec2,
-    Stack,
-    aws_s3 as s3,
-    Duration,
-    CfnOutput,
-    Fn
+    aws_ec2 as ec2,  # Alias para módulos relacionados à Amazon EC2
+    Stack,           # Classe base para todas as stacks no AWS Cloud Development Kit (CDK)
+    aws_s3 as s3,    # Alias para módulos relacionados ao Amazon Simple Storage Service (S3)
+    Duration,        # Classe para especificar durações de tempo
+    CfnOutput,       # Classe para criar saídas de dados personalizadas
+    Fn               # Classe para funções intrínsecas
 )
 
+# Importa a classe Construct do pacote constructs
 from constructs import Construct
 
+# Define a classe CrossstacksStack que herda de Stack
 class CrossstacksStack(Stack):
 
+    # Método construtor da classe CrossstacksStack
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        # Chama o método construtor da classe base Stack
         super().__init__(scope, construct_id, **kwargs)
 
+        # Inicializa o sufixo com base no ID da stack
         suffix = self.__initialize_suffix()
 
+        # Cria um novo bucket S3 com o ID "PyBucket"
         self.bucket = s3.Bucket(self, "PyBucket",
+            # Define o nome do bucket como "cool-bucket-" seguido pelo sufixo
             bucket_name=f"cool-bucket-{suffix}",
+            # Configura regras de ciclo de vida para o bucket
             lifecycle_rules=[
+                # Define uma regra de ciclo de vida para expirar os objetos após 3 dias
                 s3.LifecycleRule(
                     expiration=Duration.days(3)
                 )
             ]
         )
 
-        CfnOutput(self, "PyBucketName", 
-                  value = self.bucket.bucket_name
-                  )
+        # Cria uma saída de dados personalizada para exibir o nome do bucket criado
+        CfnOutput(self, "PyBucketName",
+                  value=self.bucket.bucket_name)
 
+    # Método privado para inicializar o sufixo com base no ID da stack
     def __initialize_suffix(self):
+        # Extrai o terceiro elemento da lista dividida pelo '/' no ID da stack
         short_stack_id = Fn.select(2, Fn.split('/', self.stack_id))
+        # Extrai o quarto elemento da lista dividida pelo '-' no ID curto da stack
         suffix = Fn.select(4, Fn.split('-', short_stack_id))
         return suffix
     
+    # Propriedade para acessar o bucket criado
     @property
     def cool_bucket(self):
         return self.bucket
--- 03-Curso_2/04-crossstacks/crossstacks/lambda_stack.py
+++ 03-Curso_2/04-crossstacks/crossstacks/lambda_stack.py
@@ -1,22 +1,32 @@
+# Importa classes e módulos específicos do pacote aws_cdk
 from aws_cdk import (
-    Stack,
-    aws_s3 as s3,
-    aws_lambda, 
+    Stack,          # Classe base para todas as stacks no AWS CDK
+    aws_s3 as s3,   # Alias para módulos relacionados ao Amazon Simple Storage Service (S3)
+    aws_lambda,     # Alias para módulos relacionados à AWS Lambda
 )
 
+# Importa a classe Construct do pacote constructs
 from constructs import Construct
 
+# Define a classe PyHandlerStack que herda de Stack
 class PyHandlerStack(Stack):
 
+    # Método construtor da classe PyHandlerStack
     def __init__(self, scope: Construct, construct_id: str, bucket : s3.Bucket, **kwargs) -> None:
+        # Chama o método construtor da classe base Stack
         super().__init__(scope, construct_id, **kwargs)
         
+        # Cria uma função Lambda chamada "PyCoolLambda"
         aws_lambda.Function(self, "PyCoolLambda",
+            # Especifica o código inline para a função Lambda
             code = aws_lambda.Code.from_inline (
-                "import os\ndef handler (event, context):\n print(os.environ['COOL_BUCKET_ARN'])"),
-                handler = "index.handler",
-                runtime = aws_lambda.Runtime.PYTHON_3_10,
-                environment = {
-                    "COOL_BUCKET_ARN": bucket.bucket_arn
-                }
-            )
+                "import os\ndef handler(event, context):\n print(os.environ['COOL_BUCKET_ARN'])"),  # Código Python inline
+            # Define o manipulador padrão da função Lambda
+            handler = "index.handler",  # O ponto de entrada da função Lambda
+            # Especifica a versão do runtime Python para a função Lambda
+            runtime = aws_lambda.Runtime.PYTHON_3_10,  # Versão do Python usada pela função Lambda
+            # Define variáveis de ambiente para a função Lambda
+            environment = {
+                "COOL_BUCKET_ARN": bucket.bucket_arn  # Passa o ARN do bucket S3 como variável de ambiente
+            }
+        )
