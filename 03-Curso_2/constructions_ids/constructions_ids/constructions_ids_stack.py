8027f32243233cda75e2cb6c912712eed374a0b5
enkumamoto <eiji.kumamoto@gmail.com>
2024-06-08 10:32:42 -0300

    Upload

--- 03-Curso_2/constructions_ids/constructions_ids/constructions_ids_stack.py
+++ 03-Curso_2/constructions_ids/constructions_ids/constructions_ids_stack.py
@@ -1,25 +1,35 @@
+# Importa módulos necessários da biblioteca AWS CDK
 from aws_cdk import (
-    aws_ec2 as ec2,
-    Stack,
-    aws_s3 as s3,
-    Duration,
-    CfnOutput,
+    aws_ec2 as ec2,  # Módulo para recursos relacionados à EC2
+    Stack,           # Classe base para todas as stacks no AWS CDK
+    aws_s3 as s3,    # Módulo para recursos relacionados ao S3
+    Duration,        # Classe para especificar durações
+    CfnOutput       # Classe para definir saídas de dados personalizadas
 )
 
+# Importa a classe Construct do pacote constructs
 from constructs import Construct
 
+# Define uma nova stack chamada ConstructionsIdsStack que herda de Stack
 class ConstructionsIdsStack(Stack):
 
+    # Método construtor da stack
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        # Chama o construtor da classe base Stack
         super().__init__(scope, construct_id, **kwargs)
 
+        # Cria um novo bucket S3 com o ID "TestBucket"
         bucket = s3.Bucket(self, "TestBucket",
-            bucket_name = "eijibucket",
-            lifecycle_rules = [
-            s3.LifecycleRule(
-                expiration = Duration.days(3)
-            )
+            # Define o nome do bucket como "eijibucket"
+            bucket_name="eijibucket",
+            # Configura regras de ciclo de vida para o bucket
+            lifecycle_rules=[
+                # Define uma regra de ciclo de vida para expirar os objetos após 3 dias
+                s3.LifecycleRule(
+                    expiration=Duration.days(3)
+                )
             ]
         )
-        CfnOutput(self, "BucketName", 
-                  value = bucket.bucket_name)
+        # Cria uma saída de dados personalizada para exibir o nome do bucket criado
+        CfnOutput(self, "BucketName",
+                  value=bucket.bucket_name)
